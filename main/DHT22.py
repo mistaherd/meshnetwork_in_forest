411bf5754d7ae877d6b2f70a94dc02424054e1fc
mistaherd <lanzbulldog22@gmail.com>
2024-02-09 17:12:03 +0000

    DHT22 code

--- main/DHT22.py
+++ main/DHT22.py
@@ -1,13 +1,16 @@
 #!/home/mistaherd/Documents/Github/meshnetwork_in_forest/env/lib/python3.11
 #liberiers
 #
-import Adafruit_DHT as dht
-from time import sleep
-##Set DATA pin
-DHT = 4
-h,t = dht.read_retry(dht.DHT22, DHT)
+import adafruit_dht 
+import board
+import time
+
+##Set DATA pin to pin 4
+dhtDevice =adafruit_dht.DHT22(board.D4)
+Temperature_c=dhtDevice.temperature
+Humidity=dhtDevice.humidity
 #Print Temperature and Humidity on Shell window
-print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t,h))
+print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(Temperature_c,humidity))
 # while True:
 #    #Read Temp and Hum from DHT22
 #    h,t = dht.read_retry(dht.DHT22, DHT)
