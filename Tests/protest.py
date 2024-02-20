052a0d018d79874eb2f659a9f83b8309ce77d84a
mistaherd <95090888+mistaherd@users.noreply.github.com>
2024-02-20 14:42:15 +0000

    some changes

--- Tests/protest.py
+++ Tests/protest.py
@@ -15,7 +15,7 @@ def Read_Memory_module():
 	units ={"K": 10e3,"M": 10e6, "G": 10e9}
 	try:
  		output=subprocess.check_output(["bash","test.sh"],universal_newlines=True)
-    		regex="\d{4}\.\[0-9]{1,3}[K,M,G]"
+    	regex="\d{4}\.\[0-9]{1,3}[K,M,G]"
    		match= re.search(regex,output)
 		if match:
 			value,unit=match.group(0).split()
---
+++ bash_scrpits/memorytest.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+
+dir_name=$(pwd)
+
+size=$(du -sh "$dir_name" | cut -f1)
+
+echo "$size"
+ 
