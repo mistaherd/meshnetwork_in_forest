92860744de058470a9268905feaead55c2de6779
mistaherd <lanzbulldog22@gmail.com>
2024-02-24 18:08:51 +0000

    libary update

--- main/Camera.py
+++ main/Camera.py
@@ -1,2 +1,10 @@
 #!/home/mistaherd/Documents/Github/meshnetwork_in_forest/env/lib/python3.11
 from picamera import PiCamera
+from time import sleep
+from datetime import datetime
+
+timestamp=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+camera=PiCamera()
+camera.start_preview()
+sleep(2)
