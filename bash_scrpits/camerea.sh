9c4696e0781bd391ad22c7b27f33db4815872969
mistaherd <lanzbulldog22@gmail.com>
2024-03-21 22:05:38 +0000

    camera commandline code

--- bash_scrpits/camerea.sh
+++ bash_scrpits/camerea.sh
@@ -1,10 +1,9 @@
 # !/bin/bash
-timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+timestamp=$(date +"%Y-%m-%d_%H:%M:%S")
 fname="camera_output_$timestamp.png"
 output_dir="Images_camera"
 if [ ! -d "$output_dir" ]; then
   # Create the directory if it doesn't exist
   mkdir -p "$output_dir"
-  echo "Created directory: $output_dir"
 fi
-echo "$timestamp"
+rpicam-still --raw -o "$output_dir/$fname"
