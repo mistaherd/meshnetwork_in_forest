5af3d9fc53e65f46a759be99dae706c75d7351fb
mistaherd <lanzbulldog22@gmail.com>
2024-03-22 12:26:42 +0000

    changes to cemera.py

--- main/Camera.py
+++ main/Camera.py
@@ -1,20 +1,23 @@
 #!/home/mistaherd/Documents/Github/meshnetwork_in_forest/env/lib/python3.11
-from picamera import PiCamera
+from picamera2 import Picamera2 ,Preview
 from time import sleep
 from datetime import datetime
 class Raspberry_Pi_VR_220:
     def __init__(self):
         """setup an instan  for the  camera"""
         self.timestamp=datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-        self.fname ='/home/mistaherd/Documents/Github/meshnetwork_in_forest/{}.png'.format(self.timestamp)
-        self.camera=PiCamera()
+        self.fname ='/home/mistaherd/Documents/Github/meshnetwork_in_forest/Images_Camera/{}.png'.format(self.timestamp)
+        self.camera=Picamera2()
+        self.camera_config=self.camera.create_preview_configuration()
+
         self.timeamount=2
     def take_pic(self)-> str:
         """this will take  a picture from camera"""
-        self.camera.start_preview()
+        self.camera.configure(self.camera_config)
+        self.camera.start_preview(Preview.QTGL)
+        self.camera.start()
         sleep(self.timeamount)
-        self.camera.capture(self.fname)
-        self.stop_preview()
+        self.camera.capture_file(self.fname)
         return self.fname
 camera=Raspberry_Pi_VR_220()
 picture=camera.take_pic()
