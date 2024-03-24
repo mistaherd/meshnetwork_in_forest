9e79f1178d93901d668435b03551aec2134f74f1
mistaherd <95090888+mistaherd@users.noreply.github.com>
2024-03-24 00:01:20 +0000

    added a  main file

--- main/main.py
+++ main/main.py
@@ -1,4 +1,7 @@
 #!/home/mistaherd/Documents/Github/meshnetwork_in_forest/env/lib/python3.11
-from DHT22 import DHT22
-data=DHT22()
-data.Read_DHT22_data()
+from memory_mangment import sensor_data,Memory_tester
+from Camera import Raspberry_Pi_VR_220
+camera=Raspberry_Pi_VR_220()
+camera.take_pic()
+sen_data=sensor_data()
+sen_data.write_append_csv()
