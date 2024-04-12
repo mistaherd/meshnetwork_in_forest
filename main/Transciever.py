6ef1b9b9e2b1f9c393b9b20cd74f45249e4dbf0f
mistaherd <95090888+mistaherd@users.noreply.github.com>
2024-04-12 16:55:49 +0100

    transcer update poorly writen code

--- main/Transciever.py
+++ main/Transciever.py
@@ -2,29 +2,37 @@
 import time
 import serial
 import threading
+from memory_mangment import sensor_data
 class Transciever:
 	def __init__(self,data):
-		self.transceive=serial.Serial(port='/dev/tty50',baudrate=9600,parity=serial.Parity_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
+		self.transceive=serial.Serial(port='/dev/tty50',
+								baudrate=9600,
+								parity=serial.Parity_NONE,
+								stopbits=serial.STOPBITS_ONE,
+								bytesize=serial.EIGHTBITS,
+								timeout=1)
 		self.message="Hello world! "
 		self.data=data
 		self.txt_fname="/home/mistaherd/Documents/Github/meshnetwork_in_forest/Tests/transmited_text.txt"
+		self.csv_fname=sensor_data().fname
 		self.recived=self.transceive.in_waiting
 		self.event=threading.Event()
 	def serial_interrupt(self):
 		if self.recived:
 			self.event.set()
-	# hello world 
+	# hello world
 	def Transmit_test_message(self):
 		"""send a simple hello world"""
 		self.transceive.write(bytes(self.message,'utf-8'))
 		time.sleep(0.2)
-	def Recive_test_message(self):
-		"""recive a simple message"""
-		self.transceive.attachInterrupt(serial_interrupt)
+	def Receive_test_message(self):
+		"""receive a simple message"""
+		self.transceive.attachInterrupt(self.serial_interrupt)
 		if self.event.is_set():
 			data_read=self.transceive.readline()
 			s=data_read.decode("utf-8")
-			print("message recived:",s)
+			print("message received:",s)
+
 	# Text file
 	def Tranmist_test_text_file(self):
 		"""Transmit a text file"""
@@ -32,12 +40,41 @@ class Transciever:
 			data=f.read()
 		self.transceive.writeline(data.encode())
 	def Recevive_test_text_file(self):
-		"""Transmit a text file"""
-		self.transceive.attachInterrupt(serial_interrupt)
-                if self.event.is_set():
+		"""receive a text file"""
+		self.transceive.attachInterrupt(self.serial_interrupt)
+		if self.event.is_set():
 			data_read=self.transceive.readline()
-                        s=data_read.decode("utf-8")
-                        print("message recived:",s)
-	#test csv file
+			s=data_read.decode("utf-8")
+			print("message received:",s)
 	
+	#test csv file
+	def Tranmist_test_csv_file(self):
+		"""Transmit a csv file"""
+		with open(self.csv_fname,'rb') as f:
+			data= f.read()
+		self.transceive.write(data)
+	def Recevive_test_csv_file(self):
+		"""receive a csv file"""
+		self.transceive.attachInterrupt(self.serial_interrupt)
+		if self.event.is_set():
+			data=self.transceive.readline()
+			s=data.decode("utf-8")
+			print("message received:",s) 
+
 	#Test png,jpg
+	def Transmit_test_png_file(self):
+		"""Transmit a PNG file"""
+		with open(self.png_fname, 'rb') as f:
+			data = f.read()
+		self.transceive.write(data)
+	def Receive_test_png_file(self):
+		"""Receive a PNG file"""
+		self.transceive.attachInterrupt(self.serial_interrupt)
+		if self.event.is_set():
+			data_read = self.transceive.readline()
+			s = data_read.decode("utf-8")
+			print("message received:", s)
+			with open(self.png_fname, 'wb') as f:
+				f.write(s)
+
+
