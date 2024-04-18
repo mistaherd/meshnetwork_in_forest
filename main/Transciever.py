67238b9cfd1720029cbc921564fd27ed85e40107
mistaherd <95090888+mistaherd@users.noreply.github.com>
2024-04-18 14:01:14 +0100

    bolier palte code before haveing this module

--- main/Transciever.py
+++ main/Transciever.py
@@ -15,37 +15,57 @@ class Transciever:
 								timeout=1)
 		self.message="Hello world! "
 		self.data=data
+		self.chunk_size=240
 		self.txt_fname="/home/mistaherd/Documents/Github/meshnetwork_in_forest/Tests/transmited_text.txt"
 		self.csv_fname=sensor_data().fname
-		self.recived=self.transceive.in_waiting
+		self.recived=self.transceive.in_waiting()
 		self.event=threading.Event()
 	def serial_interrupt(self):
 		if self.recived:
 			self.event.set()
+	def cal_bytes(self)-> int:
+		return len([bytes(self.data[i],'utf-8').hex() for i in range(len(self.data))])
+
 	# hello world
 	def Transmit_test_message(self):
 		"""send a simple hello world"""
-		self.message=list(self.message)
-		self.message=[bytes(self.message[i],'utf-8').hex() for i in range(len(self.message))]
-		self.transceive.write(",".join(self.message).encode())
+		self.data=self.message
+		# length=self.cal_bytes()
+		# self.message+str(length)
+		# self.message=list(self.message)
+
+		# self.message=[bytes(self.message[i],'utf-8').hex() for i in range(len(self.message))]
+		self.transceive.write(self.message.encode())	
 		time.sleep(0.2)
+		# self.transceive.write(",".join(self.message).encode())
+		# time.sleep(0.2)
 		
 	def Receive_test_message(self):
 		"""receive a simple message"""
 		self.transceive.attachInterrupt(self.serial_interrupt)
 		if self.event.is_set():
-			data_read=self.transceive.readline()
+
+			data_read=self.transceive.read()
 			data="".join(chr(int(data.decode().split(",")[x],16)) for x in range(len(data.decode().split(","))))
 			print("message received:",data)
 			self.event.clear()
+
 	# Text file
-	def Tranmist_test_text_file(self):
+	def Transmit_test_text_file(self):
 		"""Transmit a text file"""
 		with open(self.txt_fname,'r') as f:
-			data=list(f.read())
-		data=",".join([bytes(data[i],'utf-8').hex().encode() for i in range(len(data))])
-		self.transceive.writeline(data)
-	def Recevive_test_text_file(self):
+			self.data=list(f.read())
+			self.data=[bytes(self.data[i],'utf-8').hex().encode() for i in range(len(self.data))]
+		
+		length=self.cal_bytes()
+		# index_right=[((x+1)*(self.chunk_size))for x in range(len)]
+    # index_left=[x*(chunk_size) for x in range(number_of_loop)]
+		for x in range(length,self.chunk_size):
+			self.data[(x*self.chunk_size)+1:x*self.chunk_size]
+	
+		# data=",".join([bytes(data[i],'utf-8').hex().encode() for i in range(len(data))])
+		# self.transceive.write(self.data.encode())
+	def Received_test_text_file(self):
 		"""receive a text file"""
 		self.transceive.attachInterrupt(self.serial_interrupt)
 		while self.event.is_set():
@@ -80,9 +100,7 @@ class Transciever:
 		"""Receive a PNG file"""
 		self.transceive.attachInterrupt(self.serial_interrupt)
 		if self.event.is_set():
-			data_read = self.transceive.readlines()
-			
-			
+			data_read = self.transceive.readlines()	
 
 
 
