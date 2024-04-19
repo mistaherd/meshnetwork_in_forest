60b112497da35d865ca80ff373806dfe69bfbc3d
mistaherd <lanzbulldog22@gmail.com>
2024-04-19 17:19:32 +0100

    use this

--- Tests/test_reciver.py
+++ Tests/test_reciver.py
@@ -4,8 +4,8 @@ import serial
 import time
 
 lora = serial.Serial(port = '/dev/tty10' , baudrate = 9600, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS, timeout = 1)
-
-while True:
+t_end= time.time()+6
+while time.time()<t_end:
     data_read = lora.readline()#read data from other lora
     data = data_read.decode("utf-8")#convert byte into string
     print(data)
