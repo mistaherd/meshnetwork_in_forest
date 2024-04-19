de037ff38bef4b15df6cf21572eb945678982d3f
mistaherd <lanzbulldog22@gmail.com>
2024-04-19 16:55:37 +0100

    update

--- Tests/test_tranmiter.py
+++ Tests/test_tranmiter.py
@@ -4,9 +4,9 @@ import time
 import serial
 
 lora = serial.Serial(port='/dev/tty10',baudrate = 9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
-
-while True:
-    n = input("Enter The Message = ")#input the string
+t_end= time.time()+3
+while time.time()<t_end:
+    n = "hello world"
     b = bytes(n,'utf-8')#convert string into bytes
     s = lora.write(b)#send the data to other lora
     time.sleep(0.2)#delay of 200ms
