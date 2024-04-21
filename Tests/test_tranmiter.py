734135eb25513e2bdc856fe8f404f2c3ca850e5e
mistaherd <lanzbulldog22@gmail.com>
2024-04-21 16:52:06 +0100

    update

--- Tests/test_tranmiter.py
+++ Tests/test_tranmiter.py
@@ -5,7 +5,7 @@ import serial
 
 lora = serial.Serial(port='/dev/ttyS0',baudrate = 9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
 
-while time.time() <time.time()+6:
+while input("send messages")=='y':
     with open('transmited_text.txt', 'r') as f:
         data=f.read()
         
