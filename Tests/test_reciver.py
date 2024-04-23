3d637f5b13b8c6d7ff35fd58ba1bf0695792f434
mistaherd <lanzbulldog22@gmail.com>
2024-04-23 18:17:57 +0100

    newfiles

--- Tests/test_reciver.py
+++ Tests/test_reciver.py
@@ -5,13 +5,14 @@ import time
 import pandas as pd
 import base64
 lora = serial.Serial(port = '/dev/ttyS0' , baudrate = 9600, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS, timeout = 1)
-t_end=time.time()+6
+t_end=time.time()+3
 ouputed_data=[]
 while time.time()<t_end:
+    # print(time.time(),t_end)
     if lora.in_waiting:
         data = lora.read()
-        if data 
-        f=open('output.png','wb')
-        f.write(base64.b64decode(data))
-        # lora.write(bytes("awk",'utf-8'))
+        if data is not None:
+            f=open('output.png','wb')
+            f.write(base64.b64decode(data))
+            lora.write(bytes("awk",'utf-8'))
 
--- Tests/test_tranmiter.py
+++ Tests/test_tranmiter.py
@@ -4,14 +4,16 @@ import time
 import serial
 import base64
 lora = serial.Serial(port='/dev/ttyS0',baudrate = 9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
-t_end=time.time()+6
-while time.time()< t_end:
-    with open('/home/mistaherd/Documents/Github/meshnetwork_in_forest/Images_camera/camera_output_2024-03-21_21-43-16.png', 'rb') as f:
-        data=base64.b64encode(f.read())
-    
+t_end=time.time()+3
+length=240
+with open('/home/mistaherd/Documents/Github/meshnetwork_in_forest/Images_camera/camera_output_2024-03-21_21-43-16.png', 'rb') as f:
+        data=f.read()+b"\n EOF \n"
+        data=[data[i:i+length] for i in range(0,len(data),length)]
 
-    lora.write(data)
-    time.sleep(0.2)
-    # if lora.in_waiting:
-    #     if lora.readline().decode("utf-8") =="awk":
-    #         break
+while time.time()< t_end:
+
+    lora.write(data[0])
+    # time.sleep(0.2)
+    if lora.in_waiting:
+        if lora.readline().decode("utf-8") =="awk":
+            break
