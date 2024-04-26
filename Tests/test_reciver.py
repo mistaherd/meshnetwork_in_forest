6423918ac9b7933528174ec5b17f79aea7ade995
mistaherd <lanzbulldog22@gmail.com>
2024-04-26 14:25:47 +0100

    bug and more bugs

--- Tests/test_reciver.py
+++ Tests/test_reciver.py
@@ -5,23 +5,24 @@ import time
 import pandas as pd
 import base64
 import re
-import asyncio
-async def reciver_png():
-    lora = serial.Serial(port = '/dev/ttyS0' , baudrate = 9600, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS, timeout = 1)
-    
-    stored_data=[]
-    end_of_file=b"\n EOF \n"
+import threading
+lora = serial.Serial(port = '/dev/ttyS0' , baudrate = 9600, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS, timeout = 1)
+t_end=time.time()+6
+stored_data=[]
+end_of_file=b"\n EOF \n"
+def read_image(data):
     while True:
-        data = await lora.read(1)
-        if not data:
-            break
         if re.match(end_of_file,data):
             break
         stored_data.append(data)
-    output=b"".join(stored_data)
-    f=open('output.png','wb')
-    f.write(base64.b64decode(output))
-    f.close()
-async def main():
-    await reciver_png()
-async.run(main())
+while time.time()<t_end:
+    # print(time.time(),t_end)
+    if lora.in_waiting:
+        data = lora.read()
+        if re.match(data,b"PNG")
+        thread=threading.Thread(target=read_image,args=(data,))
+        thread.start()
+        
+        output=b"".join(stored_data)
+        f=open('output.png','wb')
+        f.write(base64.b64decode(output))
--- Tests/test_tranmiter.py
+++ Tests/test_tranmiter.py
@@ -3,17 +3,19 @@
 import time
 import serial
 import base64
+import threading
 lora = serial.Serial(port='/dev/ttyS0',baudrate = 9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
 t_end=time.time()+3
-length=240
-with open('/home/mistaherd/Documents/Github/meshnetwork_in_forest/Images_camera/camera_output_2024-03-21_21-43-16.png', 'rb') as f:
-        data=f.read()+b"\n EOF \n"
-        data=[data[i:i+length] for i in range(0,len(data),length)]
+length=250
+  
 
-def write_img(img_list:list):
-    for i in range(len(img_list)):
-        lora.write(img_list[i])
-        time.sleep(0.2)
-
-while time.time()< t_end:
-    write_img(data)
+def read_image():
+    with open('/home/mistaherd/Documents/Github/meshnetwork_in_forest/Images_camera/camera_output_2024-03-21_21-43-16.png', 'rb') as f:
+        data=f.read()
+    a=open("imagefile.txt","wb")
+    a.write(data)
+    with open("/home/mistaherd/Documents/Github/meshnetwork_in_forest/Tests/imagefile.txt",'rb') as d:
+        data=d.read()
+        lora.write(data)
+read_image()
+print("Tranmitting file will take a while")
