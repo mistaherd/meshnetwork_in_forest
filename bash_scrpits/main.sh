750f89795a8d4927b7c251aeec7516b9d81c6c8f
mistaherd <lanzbulldog22@gmail.com>
2024-05-03 01:12:30 +0100

    wow

--- bash_scrpits/main.sh
+++ bash_scrpits/main.sh
@@ -13,9 +13,9 @@ sudo chmod g+rw /dev/ttyS0
 current_time=$(date +%H:%M)
 current_hour=$(echo $current_time | cut -d: -f 1)
 previous_hour=$((current_hour-1))
-while [ "$current_hour" != "12:00" ]&&[ "$current_hour" != "09:00" ]; do
+while [$"current_hour" != "12:00" ]&&[$"current_hour" != "09:00" ]; do
  
-  if [ "$current_hour" = $((previous_hour+1)) ]; then
+  if [$"current_hour" -eq $((previous_hour+1)) ]; then
     python /home/mistaherd/Documents/Github/meshnetwork_in_forest/main/main.py
     echo "file ran successfully"
   fi
