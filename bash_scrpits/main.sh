817ada4e5f9c7b10ea32bb34ecf1a23227f03947
mistaherd <95090888+mistaherd@users.noreply.github.com>
2024-05-03 13:42:59 +0100

    chnages

--- bash_scrpits/main.sh
+++ bash_scrpits/main.sh
@@ -14,11 +14,11 @@ current_time=$(date +%H:%M)
 current_hour=$(echo $current_time | cut -d: -f 1)
 previous_hour=$((current_hour-1))
 while [$"current_hour" != "12:00" ]&&[$"current_hour" != "09:00" ]; do
- 
   if [$"current_hour" -eq $((previous_hour+1)) ]; then
-    python /home/mistaherd/Documents/Github/meshnetwork_in_forest/main/main.py
+    # python /home/mistaherd/Documents/Github/meshnetwork_in_forest/main/main.py
+    python main/main.py
     echo "file ran successfully"
   fi
-  break #becuase everyone needs a break sometime 
+  # break #becuase everyone needs a break sometime 
 done
 
--- main/main.py
+++ main/main.py
@@ -2,12 +2,12 @@
 from memory_mangment import sensor_data,Memory_tester
 from Camera_alt import camera
 from Radiomodule import Transciever as tr
-sen_data=sensor_data()
+# sen_data=sensor_data()
 camera_obj=camera()
-mem_obj=Memory_tester()
+# mem_obj=Memory_tester()
 
-sen_data.write_append_csv()
+# sen_data.write_append_csv()
 camera_obj.run
-mem_obj.error_check()
+# mem_obj.error_check()
 #Then we transmit these
 tr.transceive_choice()
