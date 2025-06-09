9056063d11143e20d602acc8b027d7aea191e43a
Mistaherd <lanzbulldog22@gmail.com>
2025-06-09 14:51:57 +0100

    fixed

--- main/Radiomodule_TCP_client.py
+++ main/Radiomodule_TCP_client.py
@@ -6,16 +6,9 @@ import os
 byte_limit=0x3FF
 load_dotenv()
 
-async def handle_request(request,client):
-    request_dict={
-        "Message":client.send(request.encode("utf-8")),# a way to test a simple send /recive
-        "Check_health":client.send(request.encode("utf-8")),# a way to test a simple send /recivetbd",# tell me about how much mem ,temp of semptem
-        "Check_camera":client.send(request.encode("utf-8")),# a way to test a simple send /recivebd",#selection of tranport vid
-        # a way to test a simple send /recivetbd",
-        "Sensor_data":client.send(request.encode("utf-8")),# a way to test a simple send /recive
-        "Help":client.send(request.encode("utf-8")),
-        "close":client.send(request.encode("utf-8"))
-    }
+def handle_request(request,client):
+    
+    client.send(request.encode("utf-8"))
 async def run_client():
     client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     server_ip=os.getenv("SERVER_IP_ADDRESS")
@@ -28,7 +21,7 @@ async def run_client():
             if request=="close":
                 
                 break
-            await handle_request(request,client)
+            handle_request(request,client)
 
             response=client.recv(byte_limit)
             if response.lower()=="closed":
